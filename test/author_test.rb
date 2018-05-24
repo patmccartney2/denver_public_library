d9ce992334db637a9b65d64fd5f40066e80bb835
Pat McCartney <patmccartney@Pats-MacBook-Pro.local>
2018-05-24 11:56:45 -0600

    independent exercises

--- test/author_test.rb
+++ test/author_test.rb
@@ -2,5 +2,7 @@ require './test/test_helper'
 
 class AuthorTest < Minitest::Test
 
-
+  def test_existence
+    book = Book.new("Douglas", "Adams", "Hitchiker's Guide", "1978")
+    assert_equal Book, book
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,18 @@
-require './test/test_helper'
+# require './test/test_helper'
 
+require "minitest/autorun"
+require 'minitest/pride'
+require 'pry'
+require './lib/book.rb'
 class BookTest < Minitest::Test
 
+  def test_existence
+    book = Book.new("Lee", "Harper", "To Kill a Mockingbird", "July 11, 1960")
+    assert_instance_of Book, book
+  end
 
+  def test_it_has_attributes
+    book = Book.new("Douglas", "Adams", "Hitchiker's Guide", "1978")
+    assert_equal book.author_first_name, "Douglas"
+  end
 end
